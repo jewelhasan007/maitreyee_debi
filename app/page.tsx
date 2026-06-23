43b22c31e8d57c7c577d818e4e0d5d764673e7d9
Jewel Hasan <jewel.hasan@cg-bd.com>
2026-06-23 17:28:07 +0600

    final

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,4 @@
-import Navigation from './components/Navigation';
-import Hero from './components/Hero';
-import About from './components/About';
-import Books from './components/Books';
-import Music from './components/Music';
-import Gallery from './components/Gallery';
-import Social from './components/Social';
-import Contact from './components/Contact';
-import Timeline from './components/Timeline';
-import Footer from './components/Footer';
+
 import AdminPanel from './components/AdminPanel';
 import Navigation_en from './components/Navigation_en';
 import Hero_en from './components/Hero_en';
@@ -15,11 +6,11 @@ import About_en from './components/About_en';
 import Books_en from './components/Books_en';
 import Music_en from './components/Music_en';
 import Gallery_en from './components/Gallery_en';
-import Social_en from './components/Social_en';
+
 import Contact_en from './components/Contact_en';
-import Timeline_en from './components/Timeline_en';
+
 import Footer_en from './components/Footer_en';
-import Timeline_en2 from './components/Timeline_en2';
+
 import Timeline_en6 from './components/Timeline_en6';
 
 export default function Home() {
